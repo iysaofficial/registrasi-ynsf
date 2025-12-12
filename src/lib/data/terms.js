12e8e310dc2c6d2cfddd9175eba87b042cf96402
iysaofficial <youngscientist.iysa@gmail.com>
2025-12-12 09:42:29 +0700

    update TnC -k

--- src/lib/data/terms.js
+++ src/lib/data/terms.js
@@ -4,17 +4,18 @@ export const indonesiaOnlineTerms = (
       Sebelum melanjutkan, harap membaca dan menyetujui syarat & ketentuan
       berikut untuk Peserta Online:
     </h5>
-    {/* <h1 className="">Peserta Online:</h1> */}
+
     <ul>
       <li>
-        Peserta yang tidak mengumpulkan dokumen persyaratan (<em>extended abstract</em>, 
-        <em> PowerPoint</em>) setelah dua kali pengingat akan dianggap mengundurkan diri 
-        secara otomatis.
+        Peserta yang tidak mengumpulkan berkas persyaratan (Full Paper) sampai
+        batas waktu yang telah ditentukan, maka tidak akan mendapatkan nilai
+        berkas dan hanya akan mendapatkan nilai pada saat sesi live judging
+        saja.
       </li>
       <li>
-        Peserta wajib menggunakan judul dan <em>extended abstract</em> yang sesuai dengan 
-        kategori yang diikuti. (Peserta internasional, wajib menggunakan bahasa Inggris untuk 
-        seluruh persyaratan dokumen yang dibutuhkan)
+        Peserta wajib menggunakan judul dan extended abstract yang sesuai dengan
+        kategori yang diikuti. (Peserta internasional, wajib menggunakan bahasa
+        Inggris untuk seluruh persyaratan dokumen yang dibutuhkan)
       </li>
       <li>
         Keputusan panitia terkait hal ini bersifat final dan tidak dapat
@@ -34,7 +35,6 @@ export const indonesiaOnlineTerms = (
   </>
 );
 
-
 export const indonesiaOfflineTerms = (
   <>
     <h5>
@@ -43,33 +43,34 @@ export const indonesiaOfflineTerms = (
     </h5>
     <ul>
       <li>
-        Peserta yang tidak mengumpulkan berkas persyaratan (<em>extended abstract</em>,
-        <em> poster</em>) setelah dua kali pengingat akan dianggap mengundurkan diri
-        secara otomatis.
+        Peserta yang tidak mengumpulkan berkas persyaratan (Full Paper) sampai
+        batas waktu yang telah ditentukan, maka tidak akan mendapatkan nilai
+        berkas dan hanya akan mendapatkan nilai pada saat sesi live judging
+        saja.
       </li>
       <li>
-        Peserta wajib menggunakan judul dan <em>extended abstract</em> yang sesuai dengan
-        kategori yang diikuti. (Peserta internasional, wajib menggunakan bahasa
-        Inggris untuk seluruh <em>requirement</em>)
+        Peserta wajib menggunakan judul dan full paper yang sesuai dengan
+        kategori yang diikuti. (untuk peserta yang mengikuti event tingkat
+        internasional, wajib menggunakan bahasa Inggris untuk seluruh
+        requirement)
       </li>
       <li>
-        Peserta diwajibkan untuk membuat poster berukuran A0, membawa produk,
-        dan juga <em>paper</em>/<em>extended abstract</em> (dalam bentuk <em>hard copy</em>) pada saat sesi
-        penjurian.
+        Peserta diwajibkan untuk membuat poster berukuran A0, membawa produk dan
+        poster tersebut pada saat sesi penjurian.
       </li>
       <li>
         Pemberian penghargaan kepada peserta akan dilakukan sesuai dengan urutan
         yang telah ditetapkan dalam susunan acara (peserta tidak diperkenankan
-        untuk meminta <em>award</em> sebelum gilirannya).
+        untuk meminta award sebelum gilirannya).
       </li>
       <li>
         Peserta diwajibkan untuk mengikuti seluruh rangkaian kegiatan sesuai
         dengan jadwal yang telah disusun oleh panitia penyelenggara.
       </li>
       <li>
-        Bagi peserta offline yang berhak atas hadiah <em>Best Booth</em>, <em>Best Poster</em>,
-        <em> Best Presentation</em>, <em>Best Project</em>, uang tunai, <em>Semi Grand Award</em>, dan 
-        <em>Grand Award</em> namun tidak dapat hadir pada acara penyerahan hadiah, maka hadiah
+        Bagi peserta offline yang berhak atas hadiah Best booth, Best Poster,
+        Best Presentation, Best Project, uang tunai, semi grand award, dan grand
+        award namun tidak dapat hadir pada acara penyerahan hadiah, maka hadiah
         yang bersangkutan dinyatakan hangus.
       </li>
       <li>
